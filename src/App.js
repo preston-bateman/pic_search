cabf2f1b49f4f05450a5585b9f9bd4b1a3b22b8d
Preston Bateman <prestonbbateman@gmail.com>
2024-11-20 22:55:49 -0600

    fixed pics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from "react"
 import SearchBar from "./components/SearchBar"
+import searchImages from "./api"
+import ImageList from "./components/ImageList"
 
 function App() {
-    const handleSubmit = (term) => {
-        console.log('do a search with', term)
+    const [images, setImages] = useState([])
+
+    const handleSubmit = async (term) => {
+        const result = await searchImages(term)
+
+        setImages(result)
+        
     }
 
     return(
         <div>
             <SearchBar onSubmit={handleSubmit} />
+            <ImageList images={images} />
         </div>
     )
 }
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,15 @@
-function ImageList() {
+import ImageShow from "./ImageShow"
+
+function ImageList({ images }) {
+    const renderedImages = images.map(
+        (image) => {
+            return <ImageShow image={image} key={image.id} />
+        }
+    )
+
     return(
         <div>
-            Image List
+            {renderedImages}
         </div>
     )
 }
